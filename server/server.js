7786728606c0c2377d485cdb12efbb9687ba9adf
Tyler Tierney <tytierney@yahoo.com>
2021-11-07 19:42:00 -0500

    Build errors

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, "../build")));
 
 app.post("/updatedata", (req, res) => {
   fs.writeFile("data.json", JSON.stringify(req.body.data), (err) => {
@@ -51,7 +51,7 @@ app.post("/api/getlinkedin", async (req, res) => {
 });
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
+  res.sendFile(path.join(__dirname, "../build", "index.html"));
 });
 
 const PORT = 8080;
